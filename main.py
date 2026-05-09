6836d7c2f9fd1d7524a4611f230661691d4a891a
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-05-09 12:00:00 +0300

    added basic todo

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from fastapi import FastAPI 
+# from fastapi import FastAPI
+# app = FastAPI()
+# @app.get('/')
+# async def root():
+#     return {'message': 'Hello FastAPI'}
 
-app = 
+from pydantic import BaseModel
+from typing import List
+
+class Task(BaseModel):
+    id: int
+    title: str
+    completed: bool = False
+    task_db = []
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+    task_db.append(task)
+    return task
+@app.get("/tasks/", response_model=List[Task])
+def get_tasks():
+    return tasks_db
